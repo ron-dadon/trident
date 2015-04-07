adeebbd1216ed3385f26c4ecbafb11927f834ae3
Ron Dadon <rondadon1@gmail.com>
2015-04-07 23:00:27 +0300

    update sample application

--- application/views/main/main_index_view.class.php
+++ application/views/main/main_index_view.class.php
@@ -15,7 +15,7 @@ class Main_Index_View extends Trident_Abstract_View
         <div class="well">
             <div class="row">
                 <div class="col-xs-12 col-lg-4 text-center text-danger" id="col_1">
-                    <h2 class="animated flipInX">
+                    <h2 class="">
                         <span class="fa-stack fa-lg">
                           <i class="fa fa-circle fa-stack-2x"></i>
                           <i class="fa fa-bolt fa-stack-1x fa-inverse"></i>
@@ -25,7 +25,7 @@ class Main_Index_View extends Trident_Abstract_View
                     <p style="font-size: 125%">Web application development made easy using wrapper classes to simplify many long code segments. With a few simple steps you can create a full functioning web application from scratch!</p>
                 </div>
                 <div class="col-xs-12 col-lg-4 text-center text-success" id="col_2">
-                    <h2 class="animated flip">
+                    <h2 class="">
                         <span class="fa-stack fa-lg">
                           <i class="fa fa-circle fa-stack-2x"></i>
                           <i class="fa fa-sitemap fa-stack-1x fa-inverse"></i>
@@ -35,7 +35,7 @@ class Main_Index_View extends Trident_Abstract_View
                     <p style="font-size: 125%">Trident was built using well known and effective design patterns such as MVC (Model View Controller), Dependency Injection and Front Controller. Combining those design principles makes a easy to maintain and scale application.</p>
                 </div>
                 <div class="col-xs-12 col-lg-4 text-center text-primary" id="col_3">
-                    <h2 class="animated flipInY">
+                    <h2 class="">
                         <span class="fa-stack fa-lg">
                           <i class="fa fa-circle fa-stack-2x"></i>
                           <i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -48,14 +48,15 @@ class Main_Index_View extends Trident_Abstract_View
         </div>
     </div>
     <script>
-        /*$('#col_1').hide();
+        $('#col_1').hide();
         $('#col_2').hide();
         $('#col_3').hide();
         $(document).ready(function() {
             $('#col_1').fadeIn(800);
-            setTimeout(function() { $('#col_2').fadeIn(800); }, 600);
-            setTimeout(function() { $('#col_3').fadeIn(800); }, 1200);
-        });*/
+            $('#col_1 h2').addClass('animated flip');
+            setTimeout(function() { $('#col_2').fadeIn(800); $('#col_2 h2').addClass('animated flip'); }, 600);
+            setTimeout(function() { $('#col_3').fadeIn(800); $('#col_3 h2').addClass('animated flip'); }, 1200);
+        });
     </script>
     <?php
         $this->include_shared_view('footer');
